2b1bf2504c0573ec4563cbde1f3d78849e363b72
augustoloyza03 <augustoloyza03@gmail.com>
2024-11-29 23:05:27 -0300

    aaa

--- autitocompu/Avanzar_derecho/simulation/qsim/work/avanzar_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity avanzar_vlg_vec_tst is
-end avanzar_vlg_vec_tst;
--- tomadecision/simulation/qsim/work/decisionfinal_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity decisionfinal_vlg_vec_tst is
-end decisionfinal_vlg_vec_tst;
